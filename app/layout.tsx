d975caa96378f8c4977de9c9a2d7b2365ef037c0
youdi323323 <kouki32312171217@gmail.com>
2025-09-10 07:07:49 +0900

    いんとろでゅーすどあなるてぃくす

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
     title: "Youdie tools",
@@ -22,6 +23,8 @@ export default function RootLayout({
 
             <body>
                 {children}
+
+                <Analytics />
             </body>
         </html>
     );
